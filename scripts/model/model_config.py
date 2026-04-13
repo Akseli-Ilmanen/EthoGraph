20577883c2d66e651dc029ac290b23e31ddf29d7
Your Name <you@example.com>
2026-04-13 15:50:58 +0200

    merge

--- scripts/model/model_config.py
+++ scripts/model/model_config.py
@@ -57,10 +57,10 @@ if __name__ == "__main__":
    trainDataReady = False
    
    # model_path = r"D:\Akseli\Code\ethograph\configs\model\Freddy_train_20251021_164220.json" # only for inference mode
-   # model_path = os.path.join(eto.get_project_root(), "configs", "model", "Ivy_train_20260202_191138_epoch-100.model")
-   model_path = r"D:\Akseli\Code\ethograph\result\Poppy_train_20260331_175332\split_1\epoch-100.model"
+   model_path = os.path.join(eto.get_project_root(), "configs", "crowlab", "model", "Ivy_train_20260202_191138_epoch-100.model")
+   # model_path = r"D:\Akseli\Code\ethograph\result\Poppy_train_20260331_175332\split_1\epoch-100.model"
 
-   target_individual = "Poppy" # predict labels for this individual
+   target_individual = "Ivy" # predict labels for this individual
    
    cp_kwargs = {
       "individuals": target_individual,
@@ -74,12 +74,14 @@ if __name__ == "__main__":
    
 
    from ethograph.utils.paths import find_config
-   mapping_file = str(find_config("mapping.txt") or os.path.join(eto.get_project_root(), "configs", "mapping.txt"))
+   mapping_file = os.path.join(eto.get_project_root(), "configs", "crowlab", "mapping_Ivy.txt") # CHANGE FOR Ivy/poppy
    
    
    nc_paths = [
-      r"D:\Akseli\AI_data\derivatives\sub-02_id-Poppy\ses-000_date-20260310_01\behav\Trial_data.nc",
-      r"D:\Akseli\AI_data\derivatives\sub-02_id-Poppy\ses-000_date-20260311_01\behav\Trial_data.nc",
+      # r"D:\Akseli\AI_data\derivatives\sub-02_id-Poppy\ses-000_date-20260308_01\behav\Trial_data.nc",
+      # r"D:\Akseli\AI_data\derivatives\sub-02_id-Poppy\ses-000_date-20260309_01\behav\Trial_data.nc",
+      # r"D:\Akseli\AI_data\derivatives\sub-02_id-Poppy\ses-000_date-20260310_01\behav\Trial_data.nc",
+      r"D:\Akseli\AI_data\derivatives\sub-01_id-Ivy\ses-000_date-20260413_01\behav\Trial_data.nc",
    ]
    
    
