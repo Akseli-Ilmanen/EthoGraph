06cf4705aa18bc521cac5f25d624b8061cd4d074
Akseli Ilmanen <akseli.ilmanen>
2026-06-16 18:48:03 +0200

    fix: linting

--- ethograph/labels/export.py
+++ ethograph/labels/export.py
@@ -170,19 +170,14 @@ def enrich_labels_df(
 
         df = pd.concat(enriched_rows, ignore_index=True)
 
-
         # Crow lab only
         if "pulse_onsets" in dt.itrial(0):
-            trial_pulse_dict = {
-                trial: dt.trial(trial).pulse_onsets.values
-                for trial in dt.trials
-            }
+            trial_pulse_dict = {trial: dt.trial(trial).pulse_onsets.values for trial in dt.trials}
 
             first_in_trial = ~df["trial"].duplicated()
             df["pulse_onsets"] = ""
-            df.loc[first_in_trial, "pulse_onsets"] = (
-                df.loc[first_in_trial, "trial"]
-                .map(lambda t: "–".join(map(str, trial_pulse_dict.get(t, []))))
+            df.loc[first_in_trial, "pulse_onsets"] = df.loc[first_in_trial, "trial"].map(
+                lambda t: "–".join(map(str, trial_pulse_dict.get(t, [])))
             )
 
     return df.sort_values(["trial", "onset_s"]).reset_index(drop=True)
