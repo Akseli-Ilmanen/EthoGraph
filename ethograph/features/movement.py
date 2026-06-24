5fcf7e803583b183f960aa72db036fa13c54b1a6
Akseli Ilmanen <akseli.ilmanen>
2026-06-24 13:59:22 +0200

    fix: angle rgb

--- ethograph/features/movement.py
+++ ethograph/features/movement.py
@@ -311,13 +311,18 @@ def get_angle_rgb(xy_pos, smooth_func=None, smoothing_params=None, input_type="p
 
     curr_angles, _ = calculate_movement_angles(xy_pos, input_type)
 
+    # Save-guard
+    curr_angles = np.clip(curr_angles, -180, 180)
+
     assert np.all((curr_angles[~np.isnan(curr_angles)] >= -180) & (curr_angles[~np.isnan(curr_angles)] <= 180)), (
         f"Angles out of expected range [-180, 180]: min={np.nanmin(curr_angles):.2f}, max={np.nanmax(curr_angles):.2f}"
     )
 
+    
+
     col_lines = np.linspace(0, len(cm) - 1, 360)
 
-    angles = np.floor(curr_angles + 180)  # NaN + 180 = NaN, stays float
+    angles = np.floor(curr_angles + 180).clip(0, 359)
     valid = ~np.isnan(angles)
 
     cmap_indices = np.full(len(angles), np.nan)
